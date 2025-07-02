bced6585e42fb057a67c3ebf6941fab3e296a6fc
Narupalle_swapna <nswapna.fullstack@gmail.com>
2025-07-02 23:43:01 +0530

    Add project files.

---
+++ Csharp_step2/class/Property.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/// <summary>
+/// by using property we can control the data
+/// get and set are the accessaries of the property
+/// by using get cam get the value  
+/// by using set we can assign the value
+/// </summary>
+class StudentDetails
+{
+    string studentname = "Swapna";
+    string studentid = "4B4";
+    string course = "BTech";
+    string branch = "ECE";
+
+    //we can call properties as a attributes
+    string StudentName { get; set; }
+    string StudentId { get; set; }
+    string Course { get; set; }
+    string Branch { get; set; }
+
+    static void Main()
+    {
+        StudentDetails studentDetails = new StudentDetails();
+        studentDetails.StudentName = "Mounika";
+        studentDetails.StudentId = "4D9";
+        studentDetails.Course = "Deplama";
+        studentDetails.Branch = "CSE";
+
+        Console.WriteLine($"studentName is: {studentDetails.StudentName}, Course is: {studentDetails.Course}");
+    }
+}
---
+++ Csharp_step2/class/fields.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+/// <summary>
+/// for non static class we can create number of instances
+/// for non static class we have to create the instance
+/// for static class no need to create the instance
+/// </summary>
+class MovieDetails
+{
+    //fields: fields are variables that belongs to the class.
+    //they represent the data that an object of the class can hold.
+    string movieName = "RRR";
+    string movieDirector = "Rajamouli";
+    string movieActor = "NTR";
+    string movieReleasedate = "22-06-2022";
+
+    void ShowaboutDevaraMovie(string movieName, string movieDirector)
+    {
+        Console.WriteLine($"{this.movieName} is directed by {this.movieDirector}");//Devara,Koratala siva
+    }
+
+    static string ShowaboutKalkiMovie(string movieName, string movieReleasedate)
+    {
+        return $"{movieName} was reliesed in {movieReleasedate}";
+    }
+
+    static void ShowMoviemessages(MovieDetails movie)
+    {
+        Console.WriteLine($"moviename:{movie.movieName}, movieActor: {movie.movieActor}");
+    }
+
+    static void ShowMoviemessages_2(MovieDetails movie2)
+    {
+        Console.WriteLine($"moviename:{movie2.movieName}, movieActor: {movie2.movieActor}");
+    }
+
+
+    static void Main()
+    {
+        MovieDetails movieDetails = new MovieDetails();
+        movieDetails.movieName = "Devara";
+        movieDetails.movieDirector = "Koratala siva";
+        movieDetails.movieActor = "Jr.NTR";
+        movieDetails.movieReleasedate = "02-10-2024";
+        //string actorName = movieDetails.movieActor;
+        //Console.WriteLine(actorName);
+
+        Console.WriteLine($"print the instance of the class: {movieDetails}");//MovieDetails
+        Console.WriteLine($"movieName: {movieDetails.movieName}");//Devara
+        Console.WriteLine($"directorName: {movieDetails.movieDirector}");//Koratala siva
+        Console.WriteLine($"actorName: {movieDetails.movieActor}");//Jr.NTR
+        Console.WriteLine($"rilieseDate: {movieDetails.movieReleasedate}");//02-10-2024
+
+
+        ShowMoviemessages(movieDetails);
+
+
+        MovieDetails movie4 = new MovieDetails()
+        {
+            movieName = "salar",
+            movieDirector = "Prasanth neel",
+            movieActor = "Prabash",
+            movieReleasedate = "02-05-2024"
+        };
+        ShowMoviemessages_2(movie4);
+
+
+        movieDetails.ShowaboutDevaraMovie(movieDetails.movieName, movieDetails.movieDirector);
+
+
+        string aboutkalkiMovie = ShowaboutKalkiMovie("Kalki", "07-05-2024");
+        Console.WriteLine(aboutkalkiMovie);
+    }
+}
